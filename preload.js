b484da726128fee1ea2d844c157dbd0974f68db4
Cekavis <cmxrynp@gmail.com>
2021-05-17 22:12:10 +0800

    增加渐慢效果和动画后随机换人效果

--- preload.js
+++ preload.js
@@ -11,59 +11,78 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log(photoPaths);
   }
 
-  var timer = -1, cnt = 0, vis = [], pos = [0, 4, 1, 3, 2];
+  var flag = 0, stop = true, cnt = 0, vis = [], pos = [0, 4, 1, 3, 2];
+
+  function nextPhoto() {
+    var newPhoto;
+    do {
+      newPhoto = photoPaths[Math.floor(Math.random()*photoPaths.length)];
+    } while (vis.includes(newPhoto));
+    var oldPhoto = document.getElementById('photo' + cnt).src;
+    if (vis.includes(oldPhoto)) vis.splice(vis.indexOf(oldPhoto, 1));
+    vis.push(newPhoto);
+    document.getElementById('unit' + cnt).style.display = "block";
+    document.getElementById('caption' + cnt).innerHTML = path.basename(newPhoto, path.extname(newPhoto));
+    document.getElementById('photo' + cnt).src = newPhoto;
+  }
 
   document.getElementById('button').onclick = function() {
     document.getElementById('file_input').style.display = "none";
-    if (timer === -1) {
-      timer = setInterval(() => {
-        var newPhoto;
-        do {
-          newPhoto = photoPaths[Math.floor(Math.random()*photoPaths.length)];
-        } while (vis.includes(newPhoto));
-        var oldPhoto = document.getElementById('photo' + cnt).src;
-        if (vis.includes(oldPhoto)) vis.splice(vis.indexOf(oldPhoto, 1));
-        vis.push(newPhoto);
-        document.getElementById('unit' + cnt).style.display = "block";
-        document.getElementById('caption' + cnt).innerHTML = path.basename(newPhoto, path.extname(newPhoto));
-        document.getElementById('photo' + cnt).src = newPhoto;
-      }, 150);
+    if (stop) {
+      stop = false;
+      flag = 100;
+      setTimeout(function change() {
+        nextPhoto();
+        if(stop) flag = flag * 1.5;
+        if(flag < 700){
+          setTimeout(() => {
+            change();
+          }, flag);
+        }
+        else{
+          setTimeout(() => {
+            document.getElementById("unit" + cnt).animate([
+              {
+                left: "40%",
+                top: "10em"
+              },{
+                left: pos[cnt]*2 + "0%",
+                top: "3em"
+              }
+            ], {
+              duration: 2000,
+              delay: 1000,
+              fill: "forwards",
+              easing: "ease",
+            })
+
+            setTimeout(() => {
+              if(cnt === 0 || Math.random()<0.5) nextPhoto();
+              ++cnt;
+              if(cnt==5){
+                document.getElementById("button").style.display = "none";
+                for (let i = 0; i < 5; i++) {
+                  document.getElementById("unit" + i).animate([
+                    {
+                      top: "3em"
+                    },{
+                      top: "10em"
+                    }
+                  ], {
+                    duration: 1000,
+                    delay: 3300+pos[i]*300,
+                    fill: "forwards",
+                    easing: "ease",
+                  });
+                }
+              }
+            }, 3000);
+          }, 700);
+        }
+      }, flag);
     }
     else {
-      clearInterval(timer);
-      timer = -1;
-      document.getElementById("unit" + cnt).animate([
-        {
-          left: "40%",
-          top: "10em"
-        },{
-          left: pos[cnt]*2 + "0%",
-          top: "3em"
-        }
-      ], {
-        duration: 2000,
-        delay: 1000,
-        fill: "forwards",
-        easing: "ease",
-      })
-      ++cnt;
-      if(cnt==5){
-        document.getElementById("button").style.display = "none";
-        for (let i = 0; i < 5; i++) {
-          document.getElementById("unit" + i).animate([
-            {
-              top: "3em"
-            },{
-              top: "10em"
-            }
-          ], {
-            duration: 1000,
-            delay: 3300+pos[i]*300,
-            fill: "forwards",
-            easing: "ease",
-          });
-        }
-      }
+      stop = true;
     }
   }
 })
